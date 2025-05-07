a27927316f67ceeea8b3cff269cfd4768402b9d9
Vitoria Tedeia <viih.tedeia@gmail.com>
2025-05-07 15:04:45 -0300

    Trabalho feito

--- App.js
+++ App.js
@@ -1,20 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  TextInput,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.view}>
+      <Text style={styles.login}>LOGIN</Text>
+      <Text style={styles.subLogin}>TO CONTINUE</Text>
+
+      <TextInput
+        style={styles.username}
+        placeholder="Username"
+        placeholderTextColor="white"
+      />
+
+      <TextInput
+        style={styles.password}
+        placeholder="Password"
+        placeholderTextColor="white"
+      />
+
+      <TouchableOpacity style={styles.btn}>
+        <Text style={styles.txtbtn}>Login</Text>
+      </TouchableOpacity>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
+  login: {
+    fontSize: 45,
+    color: "white",
+  },
+  view: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "pink",
+  },
+  subLogin: {
+    fontSize: 17,
+    color: "white",
+    fontWeight: 20,
+    marginBottom: 40,
+  },
+  username: {
+    margin: 30,
+    borderBottomWidth: 1,
+    width: 300,
+    borderBottomColor: "white",
+    padding: 5,
+  },
+  password: {
+    margin: 30,
+    borderBottomWidth: 1,
+    width: 300,
+    borderBottomColor: "white",
+    padding: 5,
+  },
+  txtbtn: {
+    color: "pink",
+  },
+  btn: {
+    width: 250,
+    padding: 10,
+    backgroundColor: "white",
+    alignItems: "center",
+    marginTop: 40,
+    borderRadius: 5,
+    shadowColor: "#000",
+    shadowRadius: 10,
+    shadowOpacity: 0.25,
+    elevation: 5,
+    shadowOffset: { width: 5, height: 5 },
   },
 });
